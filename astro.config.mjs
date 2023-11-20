1c7bdfc516cac46f2151d7ab300f6ebac6cbadd6
Gerson Garayar <garayargerson20@gmail.com>
2023-11-19 22:10:25 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        button: "storyblok/Button",
+        card: "storyblok/Card",
+        section: "storyblok/Section",
+        hero: "storyblok/Hero",
+        page: "storyblok/Page",
+      },
+    }),
+    tailwind(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+});
