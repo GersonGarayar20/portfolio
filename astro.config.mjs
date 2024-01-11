c3ef3037b9b7a42895122ce1792e73afb982076f
Gerson Garayar <garayargerson20@gmail.com>
2024-01-11 09:52:41 -0500

    feat: add storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,27 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+        section: "storyblok/Section",
+        cardList: "storyblok/CardList",
+        card: "storyblok/Card",
+      },
+    }),
+    tailwind(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
